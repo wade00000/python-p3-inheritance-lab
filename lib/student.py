06adef8cc41b57ba33b68deb8b8d9f12c3709145
WADE <MEANTOINE12@GMAIL.COM>
2025-05-12 22:10:23 +0300

    message

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,10 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name,knowledge=[]):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
+
+    def learn(self,string):
+        self.knowledge.append(string)
+        
